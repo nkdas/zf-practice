ac6d24be49efe3dd32e8ae507a2cf4f134d6dafc
Neeraj Kumar Das <nkdas91@gmail.com>
2016-03-07 10:14:53 +0530

    started session in Bootstrap Zend_Session::start()

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -1,8 +1,10 @@
 <?php
 require_once APPLICATION_PATH . '/ConfigACL.php';
+
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    public function _initSession()
+    {
+        Zend_Session::start();
+    }
 }
-
